75f99ba1745d01d93a514421016b8bce993a3985
JohnTallon <21846125-JohnTallon@users.noreply.replit.com>
2025-01-18 16:13:18 +0000

    User checkpoint: Add Azure AD authentication using MSAL.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from './App';
 import "./index.css";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+
+import { msalConfig } from "./authConfig";
+
+
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
