36cd68576c82c25b6f023680537274881d1a0ab9
John Tallon <johntallon@gmail.com>
2025-01-20 12:03:28 +0000

    Assistant checkpoint: Update login to use new window instead of popup
    Assistant generated file changes:
    - client/src/components/LoginButton.tsx: Update login to use window.open
    
    ---
    
    User prompt:
    
    I dont kwo a popup

--- client/src/components/LoginButton.tsx
+++ client/src/components/LoginButton.tsx
@@ -1,17 +1,38 @@
+
 import React from "react";
 import { useMsal } from "@azure/msal-react";
-import { loginRequest } from "../../src/authConfig";
+import { loginRequest } from "../authConfig";
+import { Button } from "./ui/button";
 
-const  LoginButton: React.FC = () => {
+const LoginButton: React.FC = () => {
     const { instance } = useMsal();
 
     const handleLogin = () => {
-        instance.loginRedirect(loginRequest).catch((e) => {
-            console.error(e);
+        const authWindow = window.open('https://login.microsoftonline.com/90f9f222-ca81-4252-ab4a-a9974c8557b2/oauth2/v2.0/authorize?' + 
+            new URLSearchParams({
+                client_id: '1b1ffb5b-8849-45d7-98c0-630b7d83c647',
+                response_type: 'code',
+                redirect_uri: `https://${window.location.host}`,
+                scope: 'User.Read'
+            }).toString(),
+            '_blank',
+            'width=600,height=600'
+        );
+
+        window.addEventListener('message', function authComplete(e) {
+            if (e.data === 'auth_complete') {
+                window.removeEventListener('message', authComplete);
+                authWindow?.close();
+                window.location.reload();
+            }
         });
     };
 
-    return <button onClick={handleLogin}>Login</button>;
+    return (
+        <Button onClick={handleLogin}>
+            Sign in with Microsoft
+        </Button>
+    );
 };
 
 export default LoginButton;
