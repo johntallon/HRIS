6ca7d916fd94e8f05ead400c53b87ac846305987
John Tallon <johntallon@gmail.com>
2025-01-20 12:09:39 +0000

    Assistant checkpoint: Reverted LoginButton to original redirect auth
    Assistant generated file changes:
    - client/src/components/LoginButton.tsx: Revert LoginButton to use redirect auth
    
    ---
    
    User prompt:
    
    Can we revert back to the change before the updates to the Loginbutton

--- client/src/components/LoginButton.tsx
+++ client/src/components/LoginButton.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../authConfig";
@@ -8,23 +7,8 @@ const LoginButton: React.FC = () => {
     const { instance } = useMsal();
 
     const handleLogin = () => {
-        const authWindow = window.open('https://login.microsoftonline.com/90f9f222-ca81-4252-ab4a-a9974c8557b2/oauth2/v2.0/authorize?' + 
-            new URLSearchParams({
-                client_id: '1b1ffb5b-8849-45d7-98c0-630b7d83c647',
-                response_type: 'code',
-                redirect_uri: `https://${window.location.host}`,
-                scope: 'User.Read'
-            }).toString(),
-            '_blank',
-            'width=600,height=600'
-        );
-
-        window.addEventListener('message', function authComplete(e) {
-            if (e.data === 'auth_complete') {
-                window.removeEventListener('message', authComplete);
-                authWindow?.close();
-                window.location.reload();
-            }
+        instance.loginRedirect(loginRequest).catch((e) => {
+            console.error(e);
         });
     };
 
